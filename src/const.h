dbdf60c5bfa98cc8b7851d145782aec5543b476a
Jan Przebor <przebot@gmail.com>
2021-08-21 21:48:18 +0200

    Literally nuked non-PSVita versions

---
+++ src/const.h
@@ -0,0 +1,4 @@
+#define SCREEN_WIDTH 980
+#define SCREEN_HEIGHT 544
+#define DEBUG_IP "192.186.0.10" // Replace with your IP when debugging
+#define DEBUG_PORT 18193 // Replace with your desired port when debugging
